f64dec093c7874a1373af1b3a996b20b1f538369
GettyGertie <gettyorawo@gmail.com>
2016-05-27 15:18:23 +0300

    Resolve merge conflicts

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,11 +9,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-<<<<<<< HEAD
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
-=======
     assert_select "title", "#{@base_title}"
->>>>>>> static-pages
   end
 
   test "should get help" do
@@ -31,18 +27,7 @@ test "should get about" do
   test "should get contact" do
     get :contact
     assert_response :success
-<<<<<<< HEAD
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
-
-  test "should get about" do
-    get static_pages_about_url
-    assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
-=======
-    assert_select "title", "Contact | #{@base_title}"
->>>>>>> static-pages
-  end
-
 end
  
