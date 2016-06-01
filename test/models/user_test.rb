038b4aa514d2dd5f64b81c564e649234d24b4afe
GettyGertie <gettyorawo@gmail.com>
2016-06-01 21:21:58 +0300

    Add minimum password length

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 def setup
-@user = User.new(name: "Example User", email: "user@example.com")
+@user = User.new(name: "Example User", email: "user@example.com",
+password: "foobar", password_confirmation: "foobar")
 end
 test "should be valid" do
 assert @user.valid?
@@ -27,5 +28,9 @@ duplicate_user = @user.dup
 duplicate_user.email = @user.email.upcase
 @user.save
 assert_not duplicate_user.valid?
+end
+test "password should have a minimum length" do
+@user.password = @user.password_confirmation = "a" * 5
+assert_not @user.valid?
 end
 end
