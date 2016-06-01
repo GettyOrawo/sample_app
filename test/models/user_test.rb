9276d7ee33c5238ec504fc7db4062ec45b9cd7b5
GettyGertie <gettyorawo@gmail.com>
2016-06-01 22:30:31 +0300

    Add password validation Exercises

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -16,7 +16,7 @@ test "email should not be too long" do
 assert_not @user.valid?
 end
 test "email validation should reject invalid addresses" do
-invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+invalid_addresses = %w[user@example,com foo@bar..com user_at_foo.org user.name@example.
 foo@bar_baz.com foo@bar+baz.com]
 invalid_addresses.each do |invalid_address|
 @user.email = invalid_address
@@ -28,6 +28,12 @@ duplicate_user = @user.dup
 duplicate_user.email = @user.email.upcase
 @user.save
 assert_not duplicate_user.valid?
+end
+test "email addresses should be saved as lower-case" do
+mixed_case_email = "Foo@ExAMPle.CoM"
+@user.email = mixed_case_email
+@user.save
+assert_equal mixed_case_email.downcase, @user.reload.email
 end
 test "password should have a minimum length" do
 @user.password = @user.password_confirmation = "a" * 5
