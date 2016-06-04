9238300a42e60d1ef0fef525a819bda3e7d59595
GettyGertie <gettyorawo@gmail.com>
2016-06-04 09:32:39 +0300

    Add successful signups

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ end
 def create
 @user = User.new(user_params)
 if @user.save
+	flash[:success] = "Welcome to the Sample App!"
 redirect_to @user
 else
 render 'new'
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,11 +5,11 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   #   assert true
   test "invalid signup information" do
 get signup_path
-assert_no_difference 'User.count' do
-post users_path, user: { name: "",
+assert_difference 'User.count', 1 do
+post_via_redirect users_path, user: {
+name: "",
 email: "user@invalid",
-password:
-"foo",
+password: "foo",
 password_confirmation: "bar" }
 end
 assert_template 'users/new'
