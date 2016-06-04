0a97df67b3b39b43ad10d65ed6b6b4b671677a1f
GettyGertie <gettyorawo@gmail.com>
2016-06-04 11:32:35 +0300

    Add login and logout form

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,14 +4,15 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   test "invalid signup information" do
-get signup_path
-assert_difference 'User.count', 1 do
-post_via_redirect users_path, user: {
-name: "",
-email: "user@invalid",
-password: "foo",
-password_confirmation: "bar" }
-end
+	get signup_path
+	assert_no_difference 'User.count', 1 do
+	post_via_redirect users_path, user:
+	 { name: "", 
+	 	email: "user@invalid", 
+	 	password: "foo", 
+	 	password_confirmation: "bar" 
+	 }
+	end
 assert_template 'users/new'
 end
 end
