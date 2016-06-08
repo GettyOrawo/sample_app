781c6dc9932876b8318b61529c02353faf323004
GettyGertie <gettyorawo@gmail.com>
2016-06-08 10:32:23 +0300

    Added friendly-fowarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
-	before_action :logged_in_user, only: [:edit, :update]
-	before_action :correct_user, only: [:edit, :update]
+before_action :logged_in_user, only: [:index, :edit, :update]
+before_action :correct_user, only: [:edit, :update]
+
+	def index
+	end
+
 	def show
 		@user = User.find(params[:id])
 	end
@@ -40,6 +44,7 @@ class UsersController < ApplicationController
 
 	def logged_in_user
 		unless logged_in?
+		store_location
 		flash[:danger] = "Please log in."
 		redirect_to login_url
 		end
