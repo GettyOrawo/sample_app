a32605d5008ed6e566f0b07f2f6003a4838f6748
GettyGertie <gettyorawo@gmail.com>
2016-06-09 12:53:23 +0300

    Testing profile microposts

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20160609062733) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_microposts_on_user_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,12 +4,18 @@ User.create!(name: "Example User",
 			password_confirmation: "foobar",
 			admin: true)
 
-			99.times do |n|
-			name = Faker::Name.name
-			email = "example-#{n+1}@railstutorial.org"
-			password = "password"
-User.create!(name: name,
-			email: email,
-			password: password,
-			password_confirmation: password)
+99.times do |n|
+	name = Faker::Name.name
+	email = "example-#{n+1}@railstutorial.org"
+	password = "password"
+	User.create!(name: name,
+				email: email,
+				password: password,
+				password_confirmation: password)
+end
+
+users = User.order(:created_at).take(6)
+50.times do
+	content = Faker::Lorem.sentence(5)
+	users.each { |user| user.microposts.create!(content: content) }
 end
