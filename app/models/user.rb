4fb366c80f83ca5cd191a8abc1e54de5c40b1a0a
GettyGertie <gettyorawo@gmail.com>
2016-06-09 10:21:52 +0300

    Adding micropostr validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+	has_many :microposts
 	attr_accessor :remember_token
 	before_save { self.email = email.downcase }
 
--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -2,8 +2,7 @@ require 'test_helper'
 class MicropostTest < ActiveSupport::TestCase
 	def setup
 		@user = users(:michael)
-		# This code is not idiomatically correct.
-		@micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+		@micropost = @user.microposts.build(content: "Lorem ipsum")
 	end
 
 	test "should be valid" do
