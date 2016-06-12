0ef2a6a1111d007a98fc3e6bb7e34839bcf60218
GettyGertie <gettyorawo@gmail.com>
2016-06-12 17:14:31 +0300

    Finished Question 2

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -8,8 +8,8 @@ assert_select "a[href=?]", root_path, count: 2
 assert_select "a[href=?]", help_path
 assert_select "a[href=?]", about_path
 assert_select "a[href=?]", contact_path
-#assert_select "a[href=?]", login_path
-#assert_select "a[href=?]", signup_path
+assert_select "a[href=?]", login_path
+assert_select "a[href=?]", signup_path
 get signup_path
 assert_select "title", full_title("Sign up")
 end
