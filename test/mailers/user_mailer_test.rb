f575b4583abd6b68271c53e0bfd889c685938467
GettyGertie <gettyorawo@gmail.com>
2016-06-15 09:27:53 +0300

    Fixing password reset errors

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -6,7 +6,7 @@ test "account_activation" do
     mail = UserMailer.account_activation(user)
     assert_equal "Account activation", mail.subject
     assert_equal [user.email], mail.to
-    assert_equal ["noreply@example.com"], mail.from
+    assert_equal ["from@example.com"], mail.from
     assert_match user.name,
     mail.body.encoded
     assert_match user.activation_token,
@@ -19,7 +19,7 @@ test "password_reset" do
     mail = UserMailer.password_reset(user)
     assert_equal "Password reset", mail.subject
     assert_equal [user.email], mail.to
-    assert_equal ["noreply@example.com"], mail.from
+    assert_equal ["from@example.com"], mail.from
     assert_match user.reset_token,
     mail.body.encoded
     assert_match CGI::escape(user.email), mail.body.encoded
