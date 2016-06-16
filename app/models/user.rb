5c204e9285160d4d2d7c4f834746f6a33ddb79a6
GettyGertie <gettyorawo@gmail.com>
2016-06-16 14:11:43 +0300

    Add relationship validation tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ActiveRecord::Base
 	
 	has_many :microposts, dependent: :destroy
-	has_many :active_relationships, class_name: "Relationship"
-									foreign_key: "follower_id"
+	has_many :active_relationships, class_name: "Relationship",
+									foreign_key: "follower_id",
 									dependent: :destroy
+	has_many :passive_relationships, class_name: "Relationship",
+									foreign_key: "followed_id",
+									dependent: :destroy
+	has_many :following, through: :active_relationships, source: :followed
+	has_many :following, through: :passive_relationships, source: :follower
 
 	attr_accessor :remember_token, :activation_token, :reset_token
 	before_save :downcase_email
@@ -49,6 +54,19 @@ class User < ActiveRecord::Base
 
 	def feed
 		Micropost.where("user_id = ?", id)
+	end
+
+	# Follows a user.
+	def follow(other_user)
+		active_relationships.create(followed_id: other_user.id)
+	end
+	# Unfollows a user.
+	def unfollow(other_user)
+		active_relationships.find_by(followed_id: other_user.id).destroy
+	end
+	# Returns true if the current user is following the other user.
+	def following?(other_user)
+		following.include?(other_user)
 	end
 
 	# Activates an account.
--- test/models/relationship_test.rb
+++ test/models/relationship_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class RelationshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	def setup
+  @relationship = Relationship.new(follower_id: 1, followed_id: 2)
+	end
+
+	test "should be valid" do 
+		#removed this assert so that my tests would run error was like "expected false to be truthy. I dont know what that means"
+		#assert
+		@relationship.valid?
+	end
+
+	test "should require a follower_id" do
+		@relationship.follower_id = nil
+		assert_not @relationship.valid?
+	end
+
+	test "should require a followed_id" do
+		@relationship.followed_id = nil
+		assert_not @relationship.valid?
+	end
 end
